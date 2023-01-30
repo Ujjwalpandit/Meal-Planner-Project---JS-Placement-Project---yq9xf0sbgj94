98d339ddc27d4636c4b97ec7ee17c204a28ab41c
Ujjwalpandit <99593805+Ujjwalpandit@users.noreply.github.com>
2023-01-30 15:45:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,200 +1,296 @@
-const heightInput = document.getElementById("height");
-const weightInput = document.getElementById("weight");
-const ageInput = document.getElementById("age");
-const genderInput = document.getElementById("gender");
-const activityInput = document.getElementById("activity");
-const submit = document.getElementById("submitBtn");
-const cardContainer = document.getElementById("cards-container");
-const mealsDetails = document.getElementById("details");
-const ingredientSection = document.getElementById("ingredients");
-const stepsSection = document.getElementById("steps");
-const equipmentSection = document.getElementById("equipment");
-const recipeSection = document.getElementById("recipe-section");
+// api key : 746b258a0e9545b39106766b99414914
+// api 2nd key : 951ad27fb20b4179af69991a03acd01d
+// api 3rd key : 328df7057dd8498fbca288d49e2b8bb6
 
-const getCalorie = () => {
-  let hv = heightInput.value;
-  let wv = weightInput.value;
-  let av = ageInput.value;
-  let gv = genderInput.value;
-  let avv = activityInput.value;
-  let bmr;
+const genrateBtn = document.querySelector("#btn");
+const heightInput = document.querySelector(".height");
+const weightInput = document.querySelector(".weight");
+const ageInput = document.querySelector(".age");
+const genderInput = document.querySelector(".gender");
+const activityInput = document.querySelector(".activity");
 
-  if (hv === "" || hv <= 0 || wv === "" || wv <= 0 || av === "" || av <= 0) {
-    alert(
-      "All input field should not be empty and should not have negetive value"
-    );
+const heightWarning = document.querySelector(".warning-height");
+const weightWarning = document.querySelector(".warning-weight");
+const ageWarning = document.querySelector(".warning-age");
+const genderWarning = document.querySelector(".warning-gender");
+const activityWarning = document.querySelector(".warning-activity");
+
+const menuCard = document.querySelector(".dummy-meal");
+const ingredientContainer = document.querySelector(".ingredientContainer");
+
+
+
+const url = {
+  base: "https://api.spoonacular.com/mealplanner/generate",
+  apiKey: "951ad27fb20b4179af69991a03acd01d",
+  apiKey2: "328df7057dd8498fbca288d49e2b8bb6"
+};
+
+const imageUrl = {
+  base: "https://webknox.com/recipeImages/",
+  size: "-556x370",
+};
+
+let Url = "https://api.spoonacular.com/mealplanner/generate?apiKey=746b258a0e9545b39106766b99414914&timeFrame=day";
+
+function takeTheValues() {
+  /*-------------warning message start-----------------*/
+  heightWarning.removeAttribute("id");
+  weightWarning.removeAttribute("id");
+  ageWarning.removeAttribute("id");
+  genderWarning.removeAttribute("id");
+  activityWarning.removeAttribute("id");
+
+  if (heightInput.value === "") {
+    heightWarning.setAttribute("id", "warning-highlite");
     return;
   }
 
-  if (gv === "female") {
-    bmr = 655.1 + 9.563 * wv + 1.85 * hv - 4.676 * av;
-  } else if (gv === "male") {
-    bmr = 66.47 + 13.75 * wv + 5.003 * hv - 6.755 * av;
+  if (weightInput.value === "") {
+    weightWarning.setAttribute("id", "warning-highlite");
+    return;
   }
 
-  // Daily Calorie Requirement
-  if (avv === "light") {
-    bmr *= 1.375;
-  } else if (avv === "moderate") {
-    bmr *= 1.55;
-  } else if (avv === "active") {
-    bmr *= 1.725;
+  if (ageInput.value === "") {
+    ageWarning.setAttribute("id", "warning-highlite");
+    return;
   }
 
-  getMeals(bmr);
-};
+  if (genderInput.value === "none") {
+    genderWarning.setAttribute("id", "warning-highlite");
+    return;
+  }
 
-const getMeals = async (bmr) => {
-  document.getElementById("loader").style.display = "block";
-  const url = `yourAPI`;
+  if (activityInput.value === "none") {
+    activityWarning.setAttribute("id", "warning-highlite");
+    return;
+  }
+  /*-------------warning message end-----------------*/
 
-  let datas;
-  await fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      datas = data;
+  /*-------------calories calculater start-----------------*/
+  let BMR;
+
+  switch (genderInput.value) {
+    case "f":
+      BMR =
+        655.1 +
+        9.563 * Number(weightInput.value) +
+        1.85 * Number(heightInput.value) -
+        4.676 * Number(ageInput.value);
+      break;
+    case "m":
+      BMR =
+        66.47 +
+        13.75 * Number(weightInput.value) +
+        5.003 * Number(heightInput.value) -
+        6.755 * Number(ageInput.value);
+      break;
+    default:
+      BMR = 0;
+      break;
+  }
+
+  let BMRmultiplier;
+  switch (activityInput.value) {
+    case "1":
+      BMRmultiplier = Math.round(BMR * 1.375);
+      break;
+    case "2":
+      BMRmultiplier = Math.round(BMR * 1.55);
+      break;
+    default:
+      BMRmultiplier = Math.round(BMR * 1.725);
+      break;
+  }
+  // sessionStorage.setItem("calories", BMRmultiplier.toString());
+  getTheDate(BMRmultiplier);
+  weightInput.value = null;
+  heightInput.value = null;
+  ageInput.value = null;
+  genderInput.value = "none";
+  activityInput.value = "none";
+  /*-------------calories calculater end-----------------*/
+}
+
+async function getTheDate(cal) {
+  let tempUrl;
+  if (cal) {
+    tempUrl = Url+`&targetCalories=${cal}`;
+  } else {
+    tempUrl = Url;
+  }
+  try {
+    const req = await fetch(tempUrl);
+    const res = await req.json();
+    // console.log(res);
+    mealPlanMuneCard(res.meals);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+  getTheDate(0);
+
+let mealTime = ["BREAKFAST", "LUNCH", "DINNER"];
+
+function mealPlanMuneCard(arr) {
+  if (document.querySelector(".meals") !== null) {
+    document.querySelector(".meals").remove();
+  }
+  let mealContainer = document.createElement("div");
+  let mealId = [];
+  mealContainer.classList = "meals";
+  arr.map((item, index) => {
+    mealId.push(item.id);
+    let mainContainer = document.createElement("div");
+    mainContainer.classList = "mainContainer";
+    let main = document.createElement("div");
+    main.classList = "meal";
+    main.setAttribute("id", `d${item.id}`);
+    let heading = document.createElement("h3");
+    heading.innerText = mealTime[index];
+    let image = document.createElement("img");
+    image.src = `${imageUrl.base}${item.id}${imageUrl.size}.${item.imageType}`;
+    image.classList = "showImage";
+    image.alt = `${item.id}`;
+    image.setAttribute("id", `i${item.id}`);
+
+    let title = document.createElement("p");
+    title.classList = "imageClass";
+    title.innerText = item.title;
+    title.setAttribute("id", `p${item.id}`);
+
+    let nutritionDisplay = document.createElement("p");
+    nutritionDisplay.classList = "imageClass colories";
+    const fetchCalories = async () => {
+      try {
+        const nutrition = await fetch(
+          `https://api.spoonacular.com/recipes/${item.id}/nutritionWidget.json?apiKey=${url.apiKey}`
+        );
+        const resNutrition = await nutrition.json();
+        nutritionDisplay.innerText = `Calories - ${resNutrition.calories.slice(
+          0,
+          -1
+        )}`;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCalories();
+    let anchor = document.createElement("a");
+    let link = document.createTextNode("GET RECIPE");
+    anchor.setAttribute("href", item.sourceUrl);
+    anchor.setAttribute("target", "_blank");
+    anchor.classList = "anchorName";
+    let subAnchor = document.createElement("div");
+    subAnchor.classList = "borderAnchor";
+    subAnchor.appendChild(anchor);
+    anchor.appendChild(link);
+    mainContainer.appendChild(heading);
+    main.appendChild(image);
+    main.appendChild(title);
+    main.appendChild(nutritionDisplay);
+    main.appendChild(subAnchor);
+    mainContainer.appendChild(main);
+    mealContainer.appendChild(mainContainer);
+    menuCard.appendChild(mealContainer);
+    mealIngredientsCard(mealId, main);
+  });
+  fetchForFirstTime(mealId[0], "ingredientWidget");
+  setTimeout(() => {
+    fetchForFirstTime(mealId[0], "equipmentWidget");
+  }, 2000);
+}
+
+function mealIngredientsCard(id, mainDiv) {
+  // console.log(id);
+  mainDiv.addEventListener("click", async (e) => {
+    console.log(e.target.id.slice(1));
+    fetchForFirstTime(e.target.id.slice(1), "ingredientWidget");
+    setTimeout(() => {
+      fetchForFirstTime(e.target.id.slice(1), "equipmentWidget");
+    }, 2000);
+    e.stopPropagation();
+  });
+}
+
+function fetchForFirstTime(id, urlQuary) {
+  // console.log(id);
+  const fetchTheData = async () => {
+    try {
+      const req = await fetch(
+        `https://api.spoonacular.com/recipes/${id}/${urlQuary}.json?apiKey=${url.apiKey}`
+      );
+      const res = await req.json();
+      if (urlQuary === "ingredientWidget") {
+        createSectionForIngredients(res["ingredients"], "ingredients");
+        // console.log(res["ingredients"]);
+      } else {
+        forEquipments(res["equipment"]);
+        // console.log(res["equipment"]);
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchTheData();
+}
+
+function createSectionForIngredients(arr, checkCondition) {
+  if (document.querySelector(".ingredients") !== null) {
+    document.querySelector(".ingredients").remove();
+  }
+  const ingDiv = document.createElement("div");
+  ingDiv.classList = "ingredients";
+  setTimeout(() => {
+    if (checkCondition === "ingredients") {
+      createListForMeal(arr, ingDiv, "ingredient", "Ingredients", "name");
+      createListForMeal(arr, ingDiv, "step", "Steps", "amount");
+    } else {
+      createListForMeal(arr, ingDiv, "equipment", "Equipments", "name");
+    }
+  }, 1000);
+
+  ingredientContainer.append(ingDiv);
+}
+
+function forEquipments(arr) {
+  const ingDiv = document.querySelector(".ingredients");
+  createListForMeal(arr, ingDiv, "equipment", "Equipments", "name");
+}
+
+function createListForMeal(arr, ingDiv, id, text, obj) {
+  console.log(arr);
+  const ingredient = document.createElement("div");
+  ingredient.classList = id + " givePadding";
+  const h4Tag = document.createElement("h4");
+  h4Tag.innerText = text;
+  const unOrderedList = document.createElement("ul");
+  if(arr.length > 0){
+    arr.map((item, index) => {
+      const li = document.createElement("li");
+      if (obj === "name") {
+        li.innerText =
+          item["name"].charAt(0).toUpperCase() + item["name"].slice(1);
+      } else {
+        li.innerText = `${item["amount"]["us"]["value"]} ${item["amount"]["us"]["unit"]}`;
+      }
+      unOrderedList.appendChild(li);
     });
-  generateMealsCard(datas);
-  document.getElementById("loader").style.display = "none";
-};
+  } else {
+    li.innerText = `No Data Available`;
+    unOrderedList.appendChild(li);
+  }
+  
+  const dummyDiv = document.createElement("div");
+  dummyDiv.classList = "checkOver-flow";
+  dummyDiv.appendChild(unOrderedList);
+  ingredient.appendChild(h4Tag);
+  ingredient.appendChild(dummyDiv);
+  ingDiv.appendChild(ingredient);
+}
+genrateBtn.addEventListener("click", takeTheValues);
 
-const generateMealsCard = (datas) => {
-  let cards = ``;
-  mealsDetails.innerHTML = `
-  <h1>Nutrients</h1>
-  <div class="d-flex justify-content-center">
-      <p class="px-2">Calories : ${datas?.nutrients?.calories}</p>
-      <p class="px-2">Carbohydrates : ${datas.nutrients?.carbohydrates}</p>
-      <p class="px-2">Fat : ${datas.nutrients?.fat}</p>
-      <p class="px-2">Protein : ${datas.nutrients?.protein}</p>
-  </div>
-  `;
-  datas.meals.map(async (data) => {
-    const url = `yourAPI`;
-    let imgURL;
-    await fetch(url)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        imgURL = data.image;
-      });
-    cards += `
-        <div class="col-md-4 d-flex justify-content-center mb-2">
-            <div class="card baseBlock" style="width: 18rem;">
-                <img src=${imgURL} class="card-img-top"
-                    alt="meal 1">
-                <div class="card-body">
-                    <h5 class="card-title">${data.title}</h5>
-                    <p>Preparation Time - ${data.readyInMinutes}</p>
-                    <button class="btn btn-outline-primary" onClick="btnRecipe(${data.id})" >Get Recipe</button>
-                </div>
-            </div>
-        </div>
-        `;
-    cardContainer.innerHTML = cards;
-  });
-};
 
-const btnRecipe = async (data) => {
-  recipeSection.innerHTML = "";
-  ingredientSection.innerHTML = "";
-  stepsSection.innerHTML = "";
-  equipmentSection.innerHTML = "";
-  const url = `yourApi`;
-  let information;
-
-  await fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      information = data;
-    });
-
-  recipeSection.textContent = `${information.title} Recipe`;
-
-  //   Ingridents
-  let htmlData = ``;
-  let inCardDiv = document.createElement("div");
-  inCardDiv.classList.add("carddesign", "card", "h-100");
-  let inCardBody = document.createElement("div");
-  inCardBody.classList.add("card-body");
-  let inOverlay = document.createElement("div");
-  inOverlay.classList.add("overlay");
-  let ul = document.createElement("ul");
-  information.extendedIngredients.map((ingre) => {
-    htmlData += `
-        <li>${ingre.original}</li>
-        `;
-  });
-  ul.innerHTML = htmlData;
-  let ingreH1 = document.createElement("h3");
-  ingreH1.textContent = "INGREDIENTS";
-  inCardBody.appendChild(inOverlay);
-  inCardBody.appendChild(ingreH1);
-  inCardBody.appendChild(ul);
-  inCardDiv.appendChild(inCardBody);
-  ingredientSection.appendChild(inCardDiv);
-
-  //   Steps
-  let stepsHtml = ``;
-  let stCardDiv = document.createElement("div");
-  stCardDiv.classList.add("carddesign", "card", "h-100");
-  let stCardBody = document.createElement("div");
-  stCardBody.classList.add("card-body");
-  let stOverlay = document.createElement("div");
-  stOverlay.classList.add("overlay");
-  let stepsOl = document.createElement("ol");
-  information.analyzedInstructions[0].steps.map((step) => {
-    stepsHtml += `
-        <li>${step.step}</li>
-        `;
-  });
-  stepsOl.innerHTML = stepsHtml;
-  let stepsH1 = document.createElement("h3");
-  stepsH1.textContent = "STEPS";
-  stCardBody.appendChild(stOverlay);
-  stCardBody.appendChild(stepsH1);
-  stCardBody.appendChild(stepsOl);
-  stCardDiv.appendChild(stCardBody);
-  stepsSection.appendChild(stCardDiv);
-
-  // equipmentSection
-  const urlEquip = `yourAPI`;
-  let equip;
-
-  await fetch(urlEquip)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      equip = data;
-    });
-
-  let equipData = ``;
-  let eqCardDiv = document.createElement("div");
-  eqCardDiv.classList.add("carddesign", "card", "h-100");
-  let eqCardBody = document.createElement("div");
-  eqCardBody.classList.add("card-body");
-  let eqOverlay = document.createElement("div");
-  eqOverlay.classList.add("overlay");
-  let equipUl = document.createElement("ul");
-  equip.equipment.map((equip) => {
-    equipData += `
-            <li>${equip.name}</li>
-            `;
-  });
-  equipUl.innerHTML = equipData;
-  let equipH1 = document.createElement("h3");
-  equipH1.textContent = "EQUIPMENT";
-  eqCardBody.appendChild(eqOverlay);
-  eqCardBody.appendChild(equipH1);
-  eqCardBody.appendChild(equipUl);
-  eqCardDiv.appendChild(eqCardBody);
-  equipmentSection.appendChild(eqCardDiv);
-};
-
-submit.addEventListener("click", getCalorie);
